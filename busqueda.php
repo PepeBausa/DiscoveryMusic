e1c49c066b01ee5b6ccec987bac5a67479f9b3e5
Pepe Bausá <PepeBausa@users.noreply.github.com>
2019-01-10 18:14:03 +0100

    dsfg

--- busqueda.php
+++ busqueda.php
@@ -76,7 +76,7 @@
       <div class="col-5 offset-2" id="Cuerpo">
         <div class="row" id= "mostrar_videos"></div>
       </div>
-      <div class="col-5" id="Cuerpo1">
+      <div class="col-5 offset-7" id="Cuerpo1">
         <div class="row" id= "mostrar_videos"></div>
       </div>
     </div>
