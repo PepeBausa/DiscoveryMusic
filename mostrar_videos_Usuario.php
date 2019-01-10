b7e2d535bb1db70d1d126efb1d3bdf8b29de32d6
Pepe Bausá <PepeBausa@users.noreply.github.com>
2019-01-10 15:41:21 +0100

    dfg

--- mostrar_videos_Usuario.php
+++ mostrar_videos_Usuario.php
@@ -7,7 +7,7 @@ $query = "SELECT * FROM $tbl_name WHERE artista= '$username'";
 $row  = array();
 $run = mysqli_query($conexion, $query);
 for ($i=0; $i < ($row[$i] =$run->fetch_assoc()); $i++) {
-print_r($row[$i][archivo]);
+//print_r($row[$i][archivo]);
 }
 echo json_encode($row, JSON_UNESCAPED_SLASHES);
 ?>
