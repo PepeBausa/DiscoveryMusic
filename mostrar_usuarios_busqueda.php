c84e036595244e970bed8738561d7f89f0884968
Pepe Bausá <PepeBausa@users.noreply.github.com>
2019-01-10 21:54:26 +0100

    sdfg

--- mostrar_usuarios_busqueda.php
+++ mostrar_usuarios_busqueda.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 require 'conexion_DB.php';
-$tbl_name = "videos";
+$tbl_name = "users";
 $busqueda = $_GET['busqueda'];
-$query = "SELECT * FROM $tbl_name WHERE artista='$username'";
+$query = "SELECT * FROM $tbl_name WHERE username='$busqueda'";
 $row  = array();
 $run = mysqli_query($conexion, $query);
 for ($i=0; $i < ($row[$i] =$run->fetch_assoc()); $i++) {
