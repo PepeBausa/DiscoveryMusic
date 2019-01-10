9c33b5079960f51fd1dcf3dc2784d2356602a000
Pepe Bausá <PepeBausa@users.noreply.github.com>
2019-01-10 22:30:13 +0100

    adsfg

--- mostrar_videos_busqueda.php
+++ mostrar_videos_busqueda.php
@@ -3,7 +3,7 @@ session_start();
 require 'conexion_DB.php';
 $tbl_name = "videos";
 $busqueda = $_GET['busqueda'];
-$query = "SELECT * FROM $tbl_name WHERE titulo='$busqueda'";
+$query = "SELECT * FROM $tbl_name WHERE titulo='%$busqueda%'";
 $row  = array();
 $run = mysqli_query($conexion, $query);
 for ($i=0; $i < ($row[$i] =$run->fetch_assoc()); $i++) {
