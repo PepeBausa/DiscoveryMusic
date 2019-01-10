d585a55f60df6644b65b21ad0cd9005b2d849d03
Pepe Bausá <PepeBausa@users.noreply.github.com>
2019-01-10 14:07:45 +0100

    sdag

--- mostrar_videos_Usuario.php
+++ mostrar_videos_Usuario.php
@@ -3,7 +3,7 @@ session_start();
 require 'conexion_DB.php';
 $tbl_name = "videos";
 $username = $_GET['username'];
-$query = "SELECT * FROM $tbl_name WHERE username=$username";
+$query = "SELECT * FROM $tbl_name WHERE artista=$username";
 $row  = array();
 $run = mysqli_query($conexion, $query);
 for ($i=0; $i < ($row[$i] =$run->fetch_assoc()); $i++) {
