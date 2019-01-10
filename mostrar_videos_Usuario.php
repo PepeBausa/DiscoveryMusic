b18f51de67132401196b82a0c2f0d99ee1d4373e
Pepe Bausá <PepeBausa@users.noreply.github.com>
2019-01-10 16:08:49 +0100

    sdfg

--- mostrar_videos_Usuario.php
+++ mostrar_videos_Usuario.php
@@ -3,7 +3,7 @@ session_start();
 require 'conexion_DB.php';
 $tbl_name = "videos";
 $username = "pepitobausa";
-$query = "SELECT * FROM $tbl_name WHERE artista='pepitobausa'";
+$query = "SELECT * FROM $tbl_name WHERE artista='$username'";
 $row  = array();
 $run = mysqli_query($conexion, $query);
 for ($i=0; $i < ($row[$i] =$run->fetch_assoc()); $i++) {
